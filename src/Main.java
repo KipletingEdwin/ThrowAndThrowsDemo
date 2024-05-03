f8521921a9275cdd70fa8b8c5f4f3a4f4498ec81
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-03 09:23:28 +0300

    add throw error

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static double divide(double a, double b){
+        if(b == 0){
+            throw  new ArithmeticException("Invalid Division");
+        } else {
+            return  a/b;
         }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(divide(10, 0));
+
     }
 }
